6259142b937f567513e77e30743bb259e589e665
Bernd <bernsahr@googlemail.com>
2018-02-28 12:03:47 +0100

    Ros Tutorials links

--- rrt_star_vehicle/src/RRT_OBJECT_ORIENTATED/Node.py
+++ rrt_star_vehicle/src/RRT_OBJECT_ORIENTATED/Node.py
@@ -3,6 +3,7 @@ Created on 31.01.2018
 
 @author: Arbeit
 '''
+import numpy as np
 
 class Node(object):
     '''
@@ -14,15 +15,45 @@ class Node(object):
         90° is like(0,1)
         ...
         270° is like (0,-1)
+		parent is the parent node
     '''
-    x_coor=0
-    y_coor=0
+    x=0
+    y=0
     
     orientation=0
-    parent=0
+    parent=null
+	
 
-    def __init__(self, params):
+    def __init__(self, x_coor, y_coor, orientation,parent):
         '''
         Constructor
         '''
-        
+		self.x=x_coor
+		self.y=y_coor
+		self.orientation=orientation
+		self.parent=parent
+	
+	def get_x(self)
+		return self.x 
+	def get_y(self)
+		return self.y
+	def get_orientation(self)
+		return self.orientation
+	def get_parent(self)
+		return self.parent
+    def set_x(self, x)
+		self.x=x
+	def set_y(self,y)
+		self.y=y
+	def set_orientation(self, orientation)
+		self.orientation=orientation
+	def set_parent(self, parent)
+		self.parent=parent
+	"""TODO hgierhin oder zum calculator?"""
+	def is_reachable(self, parent_node)
+		direction_vector_x=self.x-parent_node.x
+		direction_vector_y=self.y-parent_node.y
+		
+		cos_dir_vec= 
+	def set_orientation(self,parent_node)
+	
