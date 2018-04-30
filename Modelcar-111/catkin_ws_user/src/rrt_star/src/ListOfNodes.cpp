71db38f7a85051669c0ba59f3af3a1e7ccfdaca5
Bernd <bernsahr@googlemail.com>
2018-04-30 09:13:01 +0200

    verzweiflung pur

--- Modelcar-111/catkin_ws_user/src/rrt_star/src/ListOfNodes.cpp
+++ Modelcar-111/catkin_ws_user/src/rrt_star/src/ListOfNodes.cpp
@@ -13,8 +13,12 @@ auto size()->int{
 
 
 
-ListOfNodes::ListOfNodes() {
-	size=NUMBER_OF_NODES;
+ListOfNodes::ListOfNodes(int rows, int columns) {
+	std::list<Node>entry;
+	std::vector<std::list<Node>> row;
+	row.resize(columns, row);
+
+
 }
 
 ListOfNodes::~ListOfNodes() {
--- Modelcar-111/catkin_ws_user/src/rrt_star/src/ListOfNodes.h
+++ Modelcar-111/catkin_ws_user/src/rrt_star/src/ListOfNodes.h
@@ -5,20 +5,28 @@
  *      Author: name-not-found
  */
 #include <Node.h>
+#include <list>
 #ifndef SRC_LISTOFNODES_H_
 #define SRC_LISTOFNODES_H_
 
 class ListOfNodes {
 	int size;
+	int NUMBER_OF_ROWS;
+
+	std::vector<std::vector<std::list<Node>>> grid;
+
+
+
 public:
 	auto size()->int;
-	auto add_node(const Node&node)->void;
-	auto find_node(const Node&node)->void;
+	auto add_node(const Node &node)->void;
+	auto find_nearest_neighbour(const Node&node)->bool;
 
+	auto rewire_Nodes(Node & node)->void;
 	auto find_parents(const Node&node, std::list<Node>&parents)->bool;
 
+	ListOfNodes(int rows, int columns);
 
-	ListOfNodes();
 	virtual ~ListOfNodes();
 };
 
